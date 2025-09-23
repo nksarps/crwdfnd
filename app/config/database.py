c3eba9e575ec2ad95b18dd7819971d913c0030fa
Nana Kwaku <nksarps@gmail.com>
2025-09-23 13:19:40 +0000

    added database migrations

---
+++ app/config/database.py
@@ -0,0 +1,26 @@
+from decouple import config
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQL_ALCHEMY_DATABASE_URL = config('DB_URI')
+
+engine = sessionmaker(SQL_ALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def init_db():
+    Base.metadata.create_all(engine)
