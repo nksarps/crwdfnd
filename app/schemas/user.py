934f9acb7cce31457564007014edd5fb4e5f9665
Nana Kwaku <nksarps@gmail.com>
2025-09-25 13:30:46 +0000

    added token schema to user schemas

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -14,7 +14,16 @@ class UserCreate(UserBase):
     password: str
 
 
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
 class UserResponse(UserBase):
     id: UUID
     created_at: datetime
     updated_at: datetime
+
+
+class Token(BaseModel):
+    access_token: str  
+    token_type: str 
