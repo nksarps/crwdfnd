4e1dad28b0774e802f151a5f015896dc195b0602
Nana Kwaku <nksarps@gmail.com>
2025-09-26 09:53:09 +0000

    added email verificatin function

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,9 +1,10 @@
-from app.utils.auth import hash_password, verify_password, create_access_token
 from app.config.database import get_db
 from app.models.user import User
 from app.schemas.user import UserCreate, UserLogin, Token
+from app.utils.auth import hash_password, verify_password, create_access_token, verify_token
+from app.utils.mail import send_email_verification
 
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, status, Depends, HTTPException, BackgroundTasks, Request
 
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
@@ -16,7 +17,13 @@ router = APIRouter(
 
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
-async def register_user(body: UserCreate, db: Session = Depends(get_db)):
+async def register_user(
+    body: UserCreate, 
+    background_tasks: 
+    BackgroundTasks, 
+    request: Request,
+    db: Session = Depends(get_db)
+):
     db_user = db.query(User).filter(
         or_(User.email == body.email, User.username == body.username)
     ).first()
@@ -41,11 +48,46 @@ async def register_user(body: UserCreate, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_user)
 
+    token = create_access_token({'sub':new_user.email})
+
+    base_url = str(request.base_url).rstrip('/')
+    link = f'{base_url}/users/verify-email?token={token}'
+
+    background_tasks.add_task(
+        send_email_verification, 
+        new_user.email, 
+        new_user.full_name, 
+        link=link
+    )
+
     return new_user
 
 
+@router.get('/verify-email', status_code=status.HTTP_200_OK)
+async def verify_email(
+    token: str,
+    db: Session = Depends(get_db)
+):
+    email = verify_token(token)
+
+    user = db.query(User).filter(User.email == email).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail='Invalid token'
+        )
+    
+    user.is_verified = True
+    db.commit()
+
+    return {'message': 'Email verification successful'}
+
+
 @router.post('/login', status_code=status.HTTP_200_OK)
-async def user_login(body: UserLogin, db: Session = Depends(get_db)):
+async def user_login(
+    body: UserLogin, 
+    db: Session = Depends(get_db)
+):
     user = db.query(User).filter(User.email == body.email).first()
 
     if not user or not verify_password(body.password, user.password):
