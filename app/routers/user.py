9429e56a96bee519ddc2809cf5ec4b8bf8dabd0f
Nana Kwaku <nksarps@gmail.com>
2025-09-27 09:17:48 +0000

    added function to get current user

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,10 +1,11 @@
 from app.config.database import get_db
 from app.models.user import User
-from app.schemas.user import UserCreate, UserLogin, Token, PasswordReset
+from app.schemas.user import UserCreate, UserLogin, Token, PasswordReset, UserResponse
 from app.utils.auth import hash_password, verify_password, create_access_token, verify_token
 from app.utils.mail import send_email_verification, password_reset_mail
 
 from fastapi import APIRouter, status, Depends, HTTPException, BackgroundTasks, Request, Form
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 from sqlalchemy import or_
 from sqlalchemy.orm import Session
@@ -14,6 +15,8 @@ router = APIRouter(
     prefix='/users',
     tags=['users']
 )
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='users/login')
 
 
 @router.post('/register', status_code=status.HTTP_201_CREATED)
@@ -90,10 +93,10 @@ async def verify_email(
 
 @router.post('/login', status_code=status.HTTP_200_OK)
 async def user_login(
-    body: UserLogin, 
+    body: OAuth2PasswordRequestForm = Depends(), 
     db: Session = Depends(get_db)
 ):
-    user = db.query(User).filter(User.email == body.email).first()
+    user = db.query(User).filter(User.username == body.username).first()
 
     if not user or not verify_password(body.password, user.password):
         raise HTTPException(
@@ -109,7 +112,7 @@ async def user_login(
 
 
 @router.post('/forgot-password', status_code=status.HTTP_200_OK)
-def forgot_password(
+async def forgot_password(
     body: PasswordReset,
     background_tasks: BackgroundTasks,
     request: Request,
@@ -157,3 +160,33 @@ async def reset_password(
     db.commit()
 
     return {'message': 'Password reset successful'}
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+) -> User:
+    username = verify_token(token)
+    if not username:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid authentication credentials',
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    
+    user = db.query(User).filter(
+        or_(User.username == username, User.email == username)
+    ).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found'
+        )
+    return user
+
+@router.get('/me', status_code=status.HTTP_200_OK)
+async def read_users_me(
+    current_user: User = Depends(get_current_user)
+) -> UserResponse:
+    return current_user
